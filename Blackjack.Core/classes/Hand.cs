bff1c49071bb22461f0a0ead07f29d20705157cd
Lindsay Barton <lindsaybarton76@gmail.com>
2025-10-24 02:00:02 -0400

    Working on Splitting and Double

--- Blackjack.Core/classes/Hand.cs
+++ Blackjack.Core/classes/Hand.cs
@@ -13,11 +13,11 @@ public sealed class Hand
 
     //Clear the hand
     public void Clear() => _cards.Clear();
-    
+
     //Treat aces as 11, but then 1 as needed
     public int BestValue()
     {
-        int total = 0; 
+        int total = 0;
         int aces = 0;
 
         foreach (var c in _cards)
@@ -32,10 +32,11 @@ public sealed class Hand
 
 
     //Is the hand splittable? Splittable when the "first two cards are of the same denomination"
-    public bool IsSplittable => _cards[0].Rank == _cards[1].Rank;
+    //Also there needs to be only two cards in the hand, so you can't split after you hit without splitting first
+    public bool IsSplittable => (_cards[0].Rank == _cards[1].Rank) && (_cards.Count() == 2);
 
     //Is the hand double-down-able? The first two cards need to sum to 9, 10, or 11
-    public bool IsDouble => (_cards[0].Rank.GetValue() + _cards[1].Rank.GetValue()) is 9 or 10 or 11;
+    public bool IsDouble => (_cards[0].Rank.GetValue() + _cards[1].Rank.GetValue()) is 9 or 10 or 11 ;
 
     public bool IsBlackjack => _cards.Count == 2 && BestValue() == 21;
     public bool IsBust => BestValue() > 21;
