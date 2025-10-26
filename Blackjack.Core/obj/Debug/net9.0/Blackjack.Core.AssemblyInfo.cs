405a7c41ff06eb951941f83c55d746401623a46c
Lindsay Barton <lindsaybarton76@gmail.com>
2025-10-25 21:20:12 -0400

    Show cards visually using SVGs

--- Blackjack.Core/obj/Debug/net9.0/Blackjack.Core.AssemblyInfo.cs
+++ Blackjack.Core/obj/Debug/net9.0/Blackjack.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blackjack.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e86c3148d44d67faaeaec7094297c21777d4f6b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd3341c80eda6126524dd8b689b4ac917349f926")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blackjack.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blackjack.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
